1068e1b4a9614ce474f98d030dff52af30d17208
Nominstar <shalstjd3378@naver.com>
2021-06-16 21:27:38 +0900

    Create React Project

---
+++ erp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+// html의 body 부분이라 할 수 있다. 
+function App() {
+  return (
+    <div className="gray-background">
+      <img src={logo} lat="logo"/>
+      <h2>Hi</h2>
+    </div>
+  );
+}
+
+export default App;
